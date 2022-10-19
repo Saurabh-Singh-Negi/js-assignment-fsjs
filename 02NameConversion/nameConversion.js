414035c50c387b9ffe65813c0745e96f69925ef5
Saurabh <saurabhnegi0007@gmail.com>
2022-10-19 11:22:49 +0530

    added solution for NameConversion

--- 02NameConversion/nameConversion.js
+++ 02NameConversion/nameConversion.js
@@ -1,19 +1,64 @@
 const inp = document.getElementById("text");
 const btn = document.getElementById("convert-btn");
+const camel = document.getElementById("camel-case");
+const pascal = document.getElementById("pascal-case");
 const snake = document.getElementById("snake-case");
+const screaming_snake = document.getElementById("screaming-snake-case");
+const kebab = document.getElementById("kebab-case");
+const screaming_kebab = document.getElementById("screaming-kebab-case");
 
 let text = "";
-inp.addEventListener("change", () => {
+inp.addEventListener("change", ()=> {
     text = inp.value;
-    text = text.toLowerCase();
+    CamelCase();
+    PascalCase();
+    SnakeCase();
+    ScreamingSnakeCase();
+    KebabCase();
+    ScreamingKebabCase();
 })
 
-btn.addEventListener("click", () => {
-    //camelCase
-    console.log(text.join(" "));
+function CamelCase() {
+    let res = text.split(" ");
+    for(let i=1; i<res.length; i++) {
+        res[i] = res[i].charAt(0).toUpperCase() + res[i].slice(1);
+    }
+
+    res = res.join("");
+    camel.innerText = res;
+}
+
+function PascalCase() {
+    let res = text.split(" ");
+    for(let i=0; i<res.length; i++) {
+        res[i] = res[i].charAt(0).toUpperCase() + res[i].slice(1);
+    }
+
+    res = res.join("");
+    pascal.innerText = res;
+}
+
+function SnakeCase() {
+    let res = text;
+    res = res.replace(/\s/g, "_");
+    snake.innerText = res;
+}
+function ScreamingSnakeCase() {
+    let res = text.toUpperCase();
+    res = res.replace(/\s/g, "_");
+    screaming_snake.innerText = res;
+}
+function KebabCase() {
+    let res = text;
+    res = res.replace(/\s/g, "-");
+    kebab.innerText = res;
+}
+function ScreamingKebabCase() {
+    let res = text.toUpperCase();
+    res = res.replace(/\s/g, "-");
+    screaming_kebab.innerText = res;
+}
 
 
-    // snake case
-    snake.innerText = text.toLowerCase();
 
-})
+
